68949531128f609f11021062b5cc3c916a3f8b64
marc hudson <marchudson2601@gmail.com>
2023-01-06 00:03:53 +0000

    added more descriptive comments to the js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // Array of uppercase characters to be included in password
 const upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
+// This will take an array and return a random item from the array
 function getRandom(arr) {
   const index = Math.floor(Math.random() * arr.length);
   const character = arr[index]
@@ -20,7 +20,7 @@ function generatePassword () {
 
   // get prompts
   const passwordLength = Number(prompt("How many characters? (At least 10 characters but no more than 64)", 10))
-  // this check's that if the user input is NaN to display the message, then return 
+  // this checks that if the user input is NaN to display the message, then return 
   if (isNaN(passwordLength)) {
     alert("Need to specify a number for character count")
     return;
@@ -35,23 +35,23 @@ function generatePassword () {
     alert("Need to specify a character count greater than or equal to 10")
     return;
   }
-  //This checks if the input is more tha 64 to display message, then return 
+  //This checks if the input is more than 64 to display message, then return 
   if (passwordLength > 64) {
     alert("Need to specify a character count fewer than or equal to 64")
     return;
   }
-// These confirms the selections by the user
+  // These store the selections by the user for the character sets
   const hasLowercase = confirm("use lowercase characters");
   const hasUppercase = confirm("use uppercase characters");
   const hasNumeric = confirm("use numerical characters");
   const hasSpecial = confirm("use a special characters");
-  //This checks if all of the confirms are not selected then to display the message, then return
+  //This verifies if at least one character set is selected
   if (!hasLowercase && !hasUppercase && !hasNumeric && !hasSpecial) {
     alert("Need to specify at least one character set")
     return;
   }
 
-  
+  // this generate one of each character set type (the user selected) to ensure at least one is present before randomising
   if (hasSpecial) {
     password = password + getRandom(specialCharacters)
   }
@@ -66,7 +66,7 @@ function generatePassword () {
   }
   const howManyCharactersSoFar = password.length
 
-  // This generates a password
+  // This loops the remaining characters and gets a random string
   for (let i = 0; i < passwordLength - howManyCharactersSoFar; i++) {
     let allowedCharacters = [];
     if (hasSpecial) {
